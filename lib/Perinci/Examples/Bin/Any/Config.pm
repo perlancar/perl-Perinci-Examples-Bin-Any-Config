bf58761ea3f60219a99605655fc348f92dde4baf
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-15 20:54:10 +0700

    Release v0.001

---
+++ lib/Perinci/Examples/Bin/Any/Config.pm
@@ -0,0 +1,16 @@
+package Perinci::Examples::Bin::Any::Config;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example CLI scripts related to config files
+
+=head1 DESCRIPTION
+
+This distribution contains the following scripts:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
